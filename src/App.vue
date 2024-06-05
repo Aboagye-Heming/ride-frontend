bb7684234f15bd9f221b4d06e2824d645d970e55
Aboagye=Heming <hemingkwabena@gmail.com>
2024-06-05 02:17:12 +0000

    added tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="max-w-3xl mx-auto bg-green-100 text-center">
+      <h1>Hello, Heming</h1>
     </div>
   </header>
 
